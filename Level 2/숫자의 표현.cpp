e9a94a2c290b2625ac23e2d0ea923b854f931b36
jylee7665 <45624216+jylee7665@users.noreply.github.com>
2021-11-18 17:58:37 +0900

    Create 숫자의 표현.cpp
    https://programmers.co.kr/learn/courses/30/lessons/12924

---
+++ Level 2/숫자의 표현.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for(int i=0; i<n; i++){
+        int sum=0;
+        for(int j=i+1; j<=n; j++){
+            sum +=j;
+            if(sum==n){
+                answer++;
+                break;
+            }
+            if(sum>n) break;
+        }
+    }
+    return answer;
+}
