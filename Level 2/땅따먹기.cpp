ca8da10b71fd3a2a57de12d155fd0b563e078fa4
jylee7665 <45624216+jylee7665@users.noreply.github.com>
2021-12-06 17:17:08 +0900

    Create 땅따먹기.cpp
    https://programmers.co.kr/learn/courses/30/lessons/12913

---
+++ Level 2/땅따먹기.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(vector<vector<int> > land)
+{
+    for(int i=0; i<land.size()-1; i++){
+        land[i+1][0] += max(land[i][1],max(land[i][2],land[i][3]));
+        land[i+1][1] += max(land[i][0],max(land[i][2],land[i][3]));
+        land[i+1][2] += max(land[i][0],max(land[i][1],land[i][3]));
+        land[i+1][3] += max(land[i][1],max(land[i][2],land[i][0]));
+    }
+
+    return *max_element(land[land.size()-1].begin(),land[land.size()-1].end());
+}
